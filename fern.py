bf91ca8231ff302b7a353fc2cc2eadfadcac1f19
Erik Andersen <erand17@imada-106319.imada.sdu.dk>
2017-10-04 14:02:16 +0200

    ferny

--- fern.py
+++ fern.py
@@ -1,7 +1,9 @@
 import turtle
 
-def stem(t, length, width, segments, curve, steps):
-    if steps ==0:
+def lazy_stem(t, length, width, segments, curve, steps):
+    if length < 0.1:
+        return
+    if steps == 0:
         return
     for i in range(segments):
         t.pensize(width)
@@ -15,18 +17,36 @@ def stem(t, length, width, segments, curve, steps):
         stem(b, length*0.3, width*0.3, segments, curve, steps-1)
         width *= 0.87
         length *= 0.87
-    
+
+def stem(t, length, width, segments, curve, steps):
+    if length < 0.1:
+        return
+    if steps == 0:
+        return
+    for i in range(segments):
+        t.pensize(width)
+        t.lt(curve)
+        t.fd(length)
+        traceback=t.pos()
+        t.rt(90)
+        stem(t, length*0.3, width*0.3, segments, curve, steps-1)
+        b=t.clone()
+        b.lt(90)
+        stem(b, length*0.3, width*0.3, segments, curve, steps-1)
+        width *= 0.87
+        length *= 0.87
+
         
 
 def main():
     t=turtle.Turtle()
     t.ht()
+    turtle.tracer(1000)
     t.up()
     t.goto((-200,-100))
     t.down()
-    t.speed(0)
     #Ste,(t, length, width, segments, curve, steps)
-    stem(t,100,10, 20, 3, 4) # notice curvature of 3 degrees instead of 4.
+    stem(t,100,10, 12, 3, 5) # notice curvature of 3 degrees instead of 4.
     turtle.update()
 if __name__=="__main__":
     main()
