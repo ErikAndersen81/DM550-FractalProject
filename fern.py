9c87f44544a1160a29c6608ddcd132393f63eada
Erik Andersen <jensenerik81@gmail.com>
2017-10-06 12:10:34 +0200

    possibly final fern commit

--- fern.py
+++ fern.py
@@ -15,17 +15,43 @@ def lazy_stem(t, length, width, segments, curve, steps):
         lazy_stem(b, length*0.3, width*0.3, segments, curve, steps-1)
         width *= 0.87
         length *= 0.87
-        
+
+
+def recursive_stem(t, length, width, segments, curve, steps):
+    if steps == 0:
+        return
+    if length < 0.5:
+        return
+    for i in range(segments):
+        t.pensize(width)
+        t.lt(curve)
+        t.fd(length)
+        pos=t.pos()
+        head=t.heading()
+        t.rt(90)
+        recursive_stem(t, length*0.3, width*0.3, segments, curve, steps-1)
+        t.up()
+        t.goto(pos)
+        t.setheading(head)
+        t.down()
+        t.lt(90)
+        recursive_stem(t, length*0.3, width*0.3, segments, curve, steps-1)
+        t.up()
+        t.goto(pos)
+        t.setheading(head)
+        t.down()
+        width *= 0.87
+        length *= 0.87
 
 def main():
     t=turtle.Turtle()
     t.ht()
-    turtle.tracer(1000)
+    turtle.tracer(9000)
     t.up()
-    t.goto((-200,-100))
+    t.goto((-600,-100))
     t.down()
     #Stem(t, length, width, segments, curve, steps)
-    lazy_stem(t,100,10, 7, 3, 5) # notice curvature of 3 degrees instead of 4.
+    recursive_stem(t,200,6, 5, 3, 5) 
     turtle.update()
     
 if __name__=="__main__":
